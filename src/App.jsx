d8ad44231c1b48721357f70885808dbb582d3f4a
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2024-03-20 11:53:32 +0900

    최종코드

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,81 @@
-// import Link from "next/link";
-// import { Label } from "@/components/ui/label"
-// import { Input } from "@/components/ui/input"
-// import { Button } from "@/components/ui/button"
+import Label from "./components/Label";
+import Input from "./components/Input";
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
+  const fileInputRef = useRef(null);
+
+  const handleEmailInput = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordInput = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handlePasswordConfirmation = (e) => {
+    setPasswordConfirmation(e.target.value);
+  };
+
+  const handleNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      // 방법1 - base64 인코딩 방식
+      // const reader = new FileReader();
+
+      // reader.onloadend = () => {
+      //   setImagePreviewUrl(reader.result);
+      // };
+      // reader.readAsDataURL(file); // 파일을 읽어서 Data URL로 변환 (Base64 인코딩 포함)
+      // ------------------------------------
+
+      // 방법2 - 메모리 활용 방법
+      if (imagePreviewUrl) {
+        URL.revokeObjectURL(imagePreviewUrl);
+        setImagePreviewUrl("");
+      }
+
+      const url = URL.createObjectURL(file);
+      setImagePreviewUrl(url);
+    }
+  };
+
+  // 컴포넌트가 언마운트되면 실행되는 useEffect
+  useEffect(() => {
+    // 컴포넌트가 언마운트될 때 URL 해제
+    return () => {
+      if (imagePreviewUrl) {
+        URL.revokeObjectURL(imagePreviewUrl);
+      }
+    };
+  }, [imagePreviewUrl]);
+
+  const deletePreviewImage = () => {
+    setImagePreviewUrl("");
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ""; // 파일 입력 필드 초기화
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // 회원가입 API 요청 (+ 이미지 업로드 API 요청)
+    console.log({
+      email,
+      password,
+      passwordConfirmation,
+      nickname,
+    });
+  };
   return (
     <div className="h-dvh bg-gray-50 dark:bg-gray-950">
       <div className="py-12 sm:py-16">
@@ -19,101 +91,104 @@ export default function App() {
               </a>
             </p>
           </div>
-          <div className="space-y-6">
-            <div className="space-y-2 flex flex-col">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                placeholder="m@example.com"
-                type="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
-            </div>
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <a className="text-sm underline" href="#">
-                  Forgot password?
-                </a>
-              </div>
-              <input
-                id="password"
-                type="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="passwordConfirmation"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password confirmation
-              </label>
-              <input
-                id="passwordConfirmation"
-                required
-                type="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              />
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="nickname"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Nickname
-              </label>
-              <input
-                id="nickname"
-                placeholder="Nickname"
-                required
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              />
-            </div>
-            <div className="flex items-center space-x-6">
-              <div className="shrink-0">
-                <img
-                  id="preview_img"
-                  className="h-16 w-16 object-cover rounded-full"
-                  src="/default-profile.png"
-                  alt="Current profile photo"
+          <form onSubmit={onSubmit}>
+            <div className="space-y-6">
+              <div className="space-y-2 flex flex-col">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  placeholder="m@example.com"
+                  type="email"
+                  value={email}
+                  onChange={handleEmailInput}
+                  required={true}
                 />
               </div>
-              <label className="block">
-                <span className="sr-only">Choose profile photo</span>
-                <input
-                  type="file"
-                  className="block w-full text-sm text-slate-500
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <Label htmlFor="password">Password</Label>
+                  <a className="text-sm underline" href="#">
+                    Forgot password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required={true}
+                  value={password}
+                  autoComplete="off"
+                  onChange={handlePasswordInput}
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="passwordConfirmation">
+                  Password confirmation
+                </Label>
+                <Input
+                  id="passwordConfirmation"
+                  type="password"
+                  value={passwordConfirmation}
+                  onChange={handlePasswordConfirmation}
+                  required={true}
+                  autoComplete="off"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="nickname">Nickname</Label>
+                <Input
+                  id="nickname"
+                  placeholder="Nickname"
+                  value={nickname}
+                  onChange={handleNickname}
+                  required={true}
+                />
+              </div>
+              <div className="flex items-center space-x-6">
+                <div className="shrink-0">
+                  <img
+                    id="preview_img"
+                    className="h-16 w-16 object-cover rounded-full"
+                    src={`${
+                      imagePreviewUrl ? imagePreviewUrl : "/default-profile.png"
+                    }`}
+                    alt="Current profile photo"
+                  />
+                </div>
+                <label className="block">
+                  <span className="sr-only">Choose profile photo</span>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={handleImageChange}
+                    className="block w-full text-sm text-slate-500
                     file:mr-4 file:py-2 file:px-4
                     file:rounded-full file:border-0
                     file:text-sm file:font-semibold
                     file:bg-violet-50 file:text-violet-700
                     hover:file:bg-violet-100
                   "
-                />
-              </label>
+                  />
+                </label>
+                {imagePreviewUrl && (
+                  <button
+                    type="button"
+                    onClick={deletePreviewImage}
+                    className="bg-black dark:bg-white text-white dark:text-black inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
+                  >
+                    삭제
+                  </button>
+                )}
+              </div>
+              <div className="space-y-2">
+                <button
+                  className="bg-black dark:bg-white text-white dark:text-black inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
+                  type="submit"
+                >
+                  Sign up
+                </button>
+              </div>
             </div>
-            <div className="space-y-2">
-              <button
-                className="bg-black dark:bg-white text-white dark:text-black inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
-                type="submit"
-              >
-                Sign up
-              </button>
-            </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
